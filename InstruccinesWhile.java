399d5f707f59abaf8e5aaa3c044ec4cfecdd22ed
owen660 <jostinhuamanlazon@gmail.com>
2026-06-01 08:30:35 -0500

    Add files via upload

---
+++ InstruccinesWhile.java
@@ -0,0 +1,9 @@
+public class InstruccinesWhile {
+    public static void main (String[] args){
+        int numero = 1;
+        while( numero <= 1) {
+            System.out.println("Texto " + numero);
+            numero = numero + 1;
+        }
+    } // Fin del metodo main
+}  //Fin de la calse InstruccionesWhile
