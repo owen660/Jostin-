dbe0d4ed4fc57bad9da1a9d4a66c7d936358ea71
owen660 <jostinhuamanlazon@gmail.com>
2026-07-13 08:56:13 -0500

    Add files via upload

---
+++ DiasSemana.java
@@ -0,0 +1,13 @@
+class DiasSemana {
+    public static void main(String[] args) {
+
+        String[] dias = { "LUN", "MAR", "MIE",
+                          "JUE", "VIE", "SAB", "DOM" };
+
+        for (int indice = 0; indice < dias.length; indice++) {
+
+            System.out.println(dias[indice]);
+        }
+
+    } // Fin del método main
+} // Fin de la clase DiasSemana
---
+++ InicializacionArreglos.java
@@ -0,0 +1,48 @@
+public class InicializacionArreglos {
+
+    public static void main(String[] args) {
+
+        int[] intArreglo = new int[4];
+        double[] doubleArreglo = new double[4];
+        char[] charArreglo = new char[4];
+        boolean[] booleanArreglo = new boolean[4];
+        String[] stringArreglo = new String[4];
+
+        for(int indice = 0; indice < 4; indice++){
+            System.out.print(
+                intArreglo[indice] + "_ ");
+        }
+
+        System.out.println();
+
+        for(int indice = 0; indice < 4; indice++){
+            System.out.print(
+                doubleArreglo[indice] + "_ ");
+        }
+
+        System.out.println();
+
+        for(int indice = 0; indice < 4; indice++){
+            System.out.print(
+                charArreglo[indice] + "_ ");
+        }
+
+        System.out.println();
+
+        for(int indice = 0; indice < 4; indice++){
+            System.out.print(
+                booleanArreglo[indice] + "_ ");
+        }
+
+        System.out.println();
+
+        for(int indice = 0; indice < 4; indice++){
+            System.out.print(
+                stringArreglo[indice] + "_ ");
+        }
+
+        System.out.println();
+
+    } // Fin del método main
+
+} // Fin de la clase InicializacionArreglos
